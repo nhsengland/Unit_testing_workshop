704231017855f7c73e50fc2f278a2b274da74de6
RuKeane <ruth.keane1@nhs.net>
2024-06-07 15:03:19 +0100

    add create rule and some unit tests for it

--- example/src/example_functions.R
+++ example/src/example_functions.R
@@ -29,3 +29,71 @@ filter_section_timeband <- function(data, section_string, comparison_operator, t
   return(filtered_data)
   
 }
+
+
+
+
+
+create_rule <-
+  function(section_data,
+           conditional_formatting_type,
+           conditional_formatting_detail) {
+    #' create rule
+    #'
+    #' create rule for conditional formatting
+    #'
+    #' @param section_data data which will be conditionally formatted (tibble)
+    #' @param conditional_formatting_type type of conditional formation
+    #' either colorScale, arrows or traffic-
+    #' others could be added but would need to add additional if statements
+    #' @param conditional_formatting_detail extra information about conditional formatting
+    #' for colourScale:
+    #' - this is a vector of 2 or 3 colours as strings-
+    #' -these will be the colours used in conditional formatting
+    #' for arrows:
+    #' - this will not be used- can be NA
+    #' for traffic:
+    #' - this should be "number" or "percent" based on whether the numbers to be
+    #' conditionally formatted are numbers or percentages
+    #'
+    #' @return
+    
+    if (conditional_formatting_type == "colorScale") {
+      if (length(conditional_formatting_detail) == 3) {
+        #if colorScale with 3 colours- rule is min, median and max of data
+        
+        section_vector <-
+          section_data %>% select(-1) %>% t() %>%  as.vector()## get into a vector
+        min_point <- min(section_vector, na.rm = TRUE)
+        max_point <- max(section_vector, na.rm = TRUE)
+        median_point <- median(section_vector, na.rm = TRUE)
+        rule <- c(min_point, median_point, max_point)
+        
+      } else if (length(conditional_formatting_detail) == 2) {
+        #if colorScale with 2 colours- rule is min and max of data
+        
+        section_vector <-
+          section_data %>% select(-1) %>% t() %>%  as.vector()## get into a vector
+        min_point <- min(section_vector, na.rm = TRUE)
+        max_point <- max(section_vector, na.rm = TRUE)
+        rule <- c(min_point, max_point)
+      }
+    } else if (conditional_formatting_type == "arrows") {
+      #if arrows- rule is set to below
+      
+      rule <- c(-10000, 10,-0.5, 0.5, 10)
+    } else if (conditional_formatting_type == "traffic") {
+      if (conditional_formatting_detail == "number") {
+        #if traffic for numbers rule is set to below
+        
+        rule <- c(-10000, 0, 20)
+        
+      } else if (conditional_formatting_detail == "percent") {
+        #if traffic for percentages rule is set to below
+        
+        rule <- c(0, 0.6, 0.8)
+      }
+    }
+    return(rule)
+  }
+
--- example/tests/test_example_functions.R
+++ example/tests/test_example_functions.R
@@ -1,5 +1,5 @@
 library(testthat)
-
+library(tidyverse)
 source("../src/example_functions.R")
 
 test_that("add_two_numbers works as expected, for small numbers", {
@@ -53,5 +53,147 @@ test_that("filter_section_timeband is working", {
   
   # Assert
   expect_equal(expectedDataframe, actualDataframe)
+  
+})
+
+
+
+test_that("create_rule function is working for color scale with 2 colours", {
+
+  
+  #Arrange
+  section_data <- tibble(col1= c("a","b","c","d","e","f"), 
+                         col2= c(100,200,300,400,500,600),
+                         col3= c(1,2,3,4,5,6))
+  conditional_formatting_detail<- c("white","red")
+  conditional_formatting_type<- "colorScale"
+  
+  expected_rule<-c(1,600)
+  
+  #Act
+  rule<- create_rule(section_data, 
+              conditional_formatting_type,
+              conditional_formatting_detail)
+  #Assert
+  
+  expect_equal(rule, expected_rule)
+  
+  
+  })
+
+
+
+
+test_that("create_rule function is working for color scale with 3 colours", {
+  
+  
+  #Arrange
+  section_data <- tibble(col1= c("a","b","c","d","e","f"), 
+                         col2= c(10,20,30,40,50,60),
+                         col3= c(1,2,3,4,5,6))
+  conditional_formatting_detail<- c("white","pink","red")
+  conditional_formatting_type<- "colorScale"
+  
+  expected_rule<-c(1,8,60)
+  
+  #Act
+  rule<- create_rule(section_data, 
+                     conditional_formatting_type,
+                     conditional_formatting_detail)
+  #Assert
+  
+  expect_equal(rule, expected_rule)
+  
+  
+})
+
+
+test_that("create_rule function is working for color scale with 3 colours with some missing values", {
+  
+  
+  #Arrange
+  section_data <- tibble(col1= c("a","b","c"), 
+                         col2= c(10,20,30),
+                         col3= c(1,NA,3))
+  conditional_formatting_detail<- c("white","pink","red")
+  conditional_formatting_type<- "colorScale"
+  
+  expected_rule<-c(1,10,30)
+  
+  #Act
+  rule<- create_rule(section_data, 
+                     conditional_formatting_type,
+                     conditional_formatting_detail)
+  #Assert
+  
+  expect_equal(rule, expected_rule)
+  
+  
+})
+
+
+
+test_that("create_rule function is working for arrows", {
+  
+  
+  #Arrange
+  section_data <-NA
+  conditional_formatting_detail<- NA
+  conditional_formatting_type<- "arrows"
+  
+  expected_rule <- c(-10000, 10,-0.5, 0.5, 10)
+  
+  #Act
+  rule<- create_rule(section_data, 
+                     conditional_formatting_type,
+                     conditional_formatting_detail)
+  #Assert
+  
+  expect_equal(rule, expected_rule)
+  
+  
+})
+
+
+test_that("create_rule function is working for traffic- numbers", {
+  
+  
+  #Arrange
+  section_data <-NA
+  conditional_formatting_detail<- "number"
+  conditional_formatting_type<- "traffic"
+  
+  expected_rule <- c(-10000, 0, 20)
+  
+  #Act
+  rule<- create_rule(section_data, 
+                     conditional_formatting_type,
+                     conditional_formatting_detail)
+  #Assert
+  
+  expect_equal(rule, expected_rule)
+  
+  
+})
+
+
+test_that("create_rule function is working for traffic- percent", {
+  
+  
+  #Arrange
+  section_data <-NA
+  conditional_formatting_detail<- "percent"
+  conditional_formatting_type<- "traffic"
+  
+  rule <- c(0, 0.6, 0.8)
+  
+  #Act
+  rule<- create_rule(section_data, 
+                     conditional_formatting_type,
+                     conditional_formatting_detail)
+  #Assert
+  
+  expect_equal(rule, expected_rule)
+  
   
 })
