e2ad9cb92ae83ebad7f3b53b15d5a4d49bc42e77
Jennifer Struthers <jennifer.struthers1@nhs.net>
2024-06-07 13:32:03 +0100

    updated comments

--- example/tests/test_example_functions.R
+++ example/tests/test_example_functions.R
@@ -1,28 +1,32 @@
 library(testthat)
-library(dplyr)
-library(tidyr)
 
 source("../src/example_functions.R")
 
 test_that("add_two_numbers works as expected, for small numbers", {
+  # Arrange
   input_1 = 1
   input_2 = 5
   
   expected = 6
   
+  # Act
   actual = add_two_numbers(input_1, input_2)
   
+  # Assert
   expect_equal(expected, actual)
 })
 
 test_that("add_two_numbers works as expected, for large numbers", {
+  # Arrange
   input_1 = 10000000
   input_2 = 50000000
   
   expected = 60000000
   
+  # Act
   actual = add_two_numbers(input_1, input_2)
   
+  # Assert
   expect_equal(expected, actual)
 })
 
@@ -30,6 +34,8 @@ test_that("add_two_numbers works as expected, for large numbers", {
 
 
 test_that("filter_section_timeband is working", {
+  
+  # Arrange
   inputDataframe <- data.frame(
     Section = c("Section_5ai", "Section_3c", "Section_1a", "Section_5biv", "Section_5bii", "Section_5aiii", "Section_5bi"),
     time_band_max = c(66, NA, 99, 102, NA, 104, 65)
@@ -40,10 +46,12 @@ test_that("filter_section_timeband is working", {
     time_band_max = c(66, 102, 104)
   )
   
+  # Act
   actualDataframe <- filter_section_timeband(inputDataframe, "Section_5", ">", 65)
   
   actualDataframe <- data.frame(actualDataframe, row.names = NULL)
   
+  # Assert
   expect_equal(expectedDataframe, actualDataframe)
   
 })
